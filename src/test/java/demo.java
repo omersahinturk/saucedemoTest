604e9ca1ea8456ddfd392aa38ceb0ce8861c9c96
jaruwanp <jaruwanpatt@gmail.com>
2022-10-11 23:21:16 -0700

    delete demo.java

--- src/test/java/demo.java
+++
@@ -1,9 +0,0 @@
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class demo {
-    @Test(testName = "demo")
-    public void test01(){
-        Assert.assertTrue(true);
-    }
-}
