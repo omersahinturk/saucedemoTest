e9850d0172d101fe2dfed18259a52622203d2cc0
jaruwanp <jaruwanpatt@gmail.com>
2022-10-11 23:16:35 -0700

    US 302 is done

--- src/test/java/tests/SaucedemoTest.java
+++ src/test/java/tests/SaucedemoTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseTest;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,6 +10,7 @@ import utils.ConfigReader;
 
 public class SaucedemoTest extends BaseTest {
     SaucedemoPage page;
+    public String password = "secret_sauce";
 
     @BeforeMethod
     public void setUp(){
@@ -17,15 +19,29 @@ public class SaucedemoTest extends BaseTest {
 
     @Test(testName = "US 301 - Verify login",description = "US 301 - Verify standard_user can login with right password")
     public void test01(){
+
         String currentURL = driver.getCurrentUrl();
-        page.sendKeys(page.txtUsername,ConfigReader.readProperty("username"));
-        page.sendKeys(page.txtPassword,ConfigReader.readProperty("password"));
+        page.sendKeys(page.txtUsername,"standard_user");
+        page.sendKeys(page.txtPassword,password);
         page.takeScreenshot();
         page.click(page.btnLogin);
-        page.sleep(2000);
+        page.sleep(1000);
         page.takeScreenshot();
         //The url must be changed if logged in successfully
         page.assertNotEquals(currentURL,driver.getCurrentUrl());
+    }
+
+    @Test(testName = "US 302 - When locked out user tries to login",description = "When locked out user tries to login with right password I would like to see an error message \"Epic sadface: Sorry, this user has been locked out.")
+    public void test02(){
+        page.sendKeys(page.txtUsername,"locked_out_user");
+        page.sendKeys(page.txtPassword,password);
+        page.click(page.btnLogin);
+        String expectedText = "Epic sadface: Sorry, this user has been locked out.";
+        String err = driver.findElement(By.xpath("//h3")).getText();
+        page.assertEquals(expectedText,err);
+        page.sleep(1000);
+        page.takeScreenshot();
+
     }
 
 
