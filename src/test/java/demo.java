46adebbf4e296334b69cc1df7bda4f81b7cfbe87
jaruwanp <jaruwanpatt@gmail.com>
2022-10-11 18:56:10 -0700

    test git push

---
+++ src/test/java/demo.java
@@ -0,0 +1,9 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class demo {
+    @Test(testName = "demo")
+    public void test01(){
+        Assert.assertTrue(true);
+    }
+}
